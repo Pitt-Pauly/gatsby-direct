e1199bfa0ce040dd63bc83c6e3e051d558f9a6da
Pitt Pauly <pitt.pauly@gmail.com>
2019-03-06 15:15:51 +0000

    added keybase icon and link to profile

--- components/SiteLinks/index.jsx
+++ components/SiteLinks/index.jsx
@@ -8,54 +8,63 @@ class SiteLinks extends React.Component {
     return (
       <div className="blog-links">
         <ul>
-          {config.siteLinkedInUrl && (
+          {config.siteKeyBase && (
             <li>
-              <a href={config.siteLinkedInUrl}><i className="fa fa-linkedin-square" /></a>
-            </li>
-          )}
-          {config.siteTwitterUrl && (
-            <li>
-              <a href={config.siteTwitterUrl}>
-                <i className="fa fa-twitter" />
-              </a>
-            </li>
-          )}         
-          {config.siteEmail && (
-            <li>
-              <a href={`mailto:${config.siteEmail}`}>
-                <i className="fa fa-envelope-o" />
-              </a>
-            </li>
-          )}
-        </ul>
-        <ul>
-         {config.siteBlog && (
-            <li>
-              <a href={config.siteBlog}>
-                <i className="fa fa-medium" />
+              <a href={config.siteKeyBase}>
+                <i className="fa fa-keybase" aria-hidden="true" />
               </a>
             </li>
           )}
           {config.siteGithubUrl && (
             <li>
               <a href={config.siteGithubUrl}>
-                <i className="fa fa-github-alt" />
+                <i className="fa fa-github-alt" aria-hidden="true" />
               </a>
             </li>
           )}
           {config.siteStackOverflow && (
             <li>
               <a href={config.siteStackOverflow}>
-                <i className="fa fa-stack-overflow" />
+                <i className="fa fa-stack-overflow" aria-hidden="true" />
               </a>
             </li>
           )}
         </ul>
         <ul>
-        {config.siteUKC && (
+          {config.siteLinkedInUrl && (
+            <li>
+              <a href={config.siteLinkedInUrl}>
+                <i className="fa fa-linkedin-square" aria-hidden="true" />
+              </a>
+            </li>
+          )}
+          {config.siteTwitterUrl && (
+            <li>
+              <a href={config.siteTwitterUrl}>
+                <i className="fa fa-twitter" aria-hidden="true" />
+              </a>
+            </li>
+          )}
+          {config.siteEmail && (
+            <li>
+              <a href={`mailto:${config.siteEmail}`}>
+                <i className="fa fa-envelope-o" aria-hidden="true" />
+              </a>
+            </li>
+          )}
+        </ul>
+        <ul>
+          {config.siteBlog && (
+            <li>
+              <a href={config.siteBlog}>
+                <i className="fa fa-medium" aria-hidden="true" />
+              </a>
+            </li>
+          )}
+          {config.siteUKC && (
             <li>
               <a href={config.siteUKC}>
-                <i className="fa fa-picture-o" />
+                <i className="fa fa-picture-o" aria-hidden="true" />
               </a>
             </li>
           )}
